9841124d2da1dc3ed5712c6449cb769edc4ec6c3
Shehroz <mshehroz302@gmail.com>
2023-12-10 23:41:02 +0500

    Page setting

--- src/Container/NotFound404.tsx
+++ src/Container/NotFound404.tsx
@@ -12,18 +12,18 @@ const NotFound404 = () => {
           maxHeight: "full",
         }}
       >
-        <div className="text-center px-20 py-8 rounded-xl bg-white shadow-inner">
+        <div className="text-center px-20 py-8 rounded-xl bg-slate-900 bg-opacity-90 shadow-inner">
           <p className="text-xl font-normal text-red-500">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-[#343767] sm:text-5xl">
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-[#8f94d3] sm:text-5xl">
             Page not found
           </h1>
-          <p className="mt-6 text-base leading-7 text-[#67748e]">
+          <p className="mt-6 text-base leading-7 text-[#e0e2e7]">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="#"
-              className="rounded-full bg-black px-3.5 py-2.5 text-sm font-semibold border border-solid border-gray-500 text-white hover:text-black shadow-sm hover:bg-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-full bg-black px-3.5 py-2.5 text-sm font-semibold border border-solid border-black hover:border-slate-500 text-white shadow-sm hover:bg-transparent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Go back home
             </a>
