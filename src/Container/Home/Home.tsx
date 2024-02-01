baaad1fd926642cac060f59591cdec8fe4a94efe
Shehroz <mshehroz302@gmail.com>
2024-02-01 23:33:49 +0500

    Slider create
    Co-Authored-By: Maryam <152300548+Maryamhameed1@users.noreply.github.com>

--- src/Container/Home/Home.tsx
+++ src/Container/Home/Home.tsx
@@ -1,11 +1,12 @@
 import Footer from "../../Component/Footer";
 import NavBar from "../../Component/NavBar";
+import MySlider from "../../Component/Slider";
 
 const Home = () => {
   return (
     <>
       <NavBar />
-      <br />
+      <MySlider />
       <Footer />
     </>
   );
